9559289956798e1c0d8da77133309122bbaaa0bf
Hamzehomat <146542807+Hamzehomat@users.noreply.github.com>
2024-02-24 16:51:15 +0300

    react_task3

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import './Navbar.modulee.css'; 
+function Navbar() {
+  return (
+    <header className='header'>
+      <div className='container'>
+        <div className='row'>
+          <div className='logoContainer'>
+{        /*   <img src="assets/img/public/logo.svg" alt="Logo" />
+  */}        
+      <a href="#">mangcoding Store</a>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="#" className='active'>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#">Course</a>
+              </li>
+              <li>
+                <a href="#">About us</a>
+              </li>
+              <li>
+                <a href="#">Article</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <div className='authContainer'>
+            <a href="#" className='login'>
+              Log in
+            </a>
+            <a href="#" className='register'>
+              Register
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
